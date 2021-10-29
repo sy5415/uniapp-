4b4cd26c505b510cbd8fdb30695414564ca80fe6
sy <1134119490@qq.com>
2021-10-29 20:31:37 +0800

    首页完成提交

---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    9999
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
